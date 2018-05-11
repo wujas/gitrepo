8a4b92ad89a0f5e597f6c84f68846b6a008d37b5
wujas <loagn2k@gmail.com>
2018-05-11 12:03:13 +0200

    canvas

--- bootstrap4/js/canvas.js
+++ bootstrap4/js/canvas.js
@@ -1,48 +1,41 @@
 /*jshint esversion: 6*/
+const cv = document.getElementById('canvas');
+const ctx = cv.getContext('2d');
 
-const canvasElem = document.getElementById('canvas');
-const ctx = canvasElem.getContext('2d');
-
-var szer = canvasElem.width, wys = canvasElem.height;
 //kolor wypełnienia
-ctx.fillStyle = "#8f14b8";
+ctx.fillStyle = "black";
 //rysujemy prostokąt/kwadraty
-// x, y, width, height
+//x, y, width, height
 ctx.fillRect(25, 25, 100, 100);
 
 //kolor obrysu
-ctx.fillStroke = "#cbcb18";
-//drugi obrys prostokąta
-ctx.strokeRect(30, 30, 100, 100);
+ctx.fillStroke = "green";
 
-//czyszczenia obrazu
+//drugi prostokąt
+ctx.strokeRect(25, 25, 100, 100);
+
+//czyszczenie obszaru
 ctx.clearRect(45, 45, 60, 60);
 
 //rysowanie linii
-// console.log(canvasElem.width / 2, canvasElem.height / 2);
-ctx.moveTo(0, 0);
-ctx.lineTo(canvasElem.width, canvasElem.height);
-ctx.stroke();
-ctx.moveTo(0, canvasElem.height);
-ctx.lineTo( canvasElem.width, 0);
+ctx.fillStyle = "gray";
+ctx.moveTo(cv.width / 2, cv.height / 2);
+ctx.lineTo(0, 0);
+ctx.lineTo(400, 400);
+ctx.lineTo(400, 0);
+ctx.lineTo(0, 400);
 ctx.stroke();
 
-
-//rysowanie kołaf
+//rysowanie koła
 ctx.beginPath();
-// x, y, radius, start, tre/false
-ctx.arc(szer / 2, wys / 2, 100, 0, 2 * Math.PI);
-ctx.arc(szer / 2, wys / 2, 90, 0,  Math.PI);
-
+//x, y, radius, start, stop, true/false(zgodnie z ruchem wskazówek zegara lub nie)
+ctx.arc(cv.width / 2, cv.height / 2, 100, 0,2*Math.PI);
+ctx.arc(cv.width / 2, cv.height / 2, 90, 0, Math.PI);
+ctx.arc(cv.width / 2, cv.height - 50, 30, 0,2*Math.PI);
+ctx.arc(cv.width / 2, 50, 30, 0,2*Math.PI);
 ctx.stroke();
-
 ctx.beginPath();
-ctx.arc(200, 100, 70, 0, 2 * Math.PI);
+ctx.lineWidth = 5;
+ctx.arc(50, cv.height / 2, 30, 0,2*Math.PI);
+ctx.arc(350, cv.height / 2, 30, 0,2*Math.PI);
 ctx.stroke();
-
-// tekst
-ctx.lineWidth = 1;
-ctx.font = "normal 40px Arial";
-ctx.textBaseline = "middle";
-ctx.textAlign = "left";
-ctx.strokeText('$$$$$$$$', 70, 180);
