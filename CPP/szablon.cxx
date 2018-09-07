c94babcee94bc39030369933a941c5095ff0af5c
wujas <loagn2k@gmail.com>
2018-09-07 14:19:04 +0200

    python i c++

---
+++ CPP/szablon.cxx
@@ -0,0 +1,20 @@
+/*
+ * szablon.cxx
+ */
+
+
+#include <iostream>
+
+using namespace std; 
+
+int main(int argc, char **argv)
+{
+    int a; // deklaracja zmiennej
+    a = 0; // inicjacja zmiennej
+    cout << "Podaj liczbę: ";
+    cin >> a;
+    cout << a; 
+    
+	return 0;
+}
+
