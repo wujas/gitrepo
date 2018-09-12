dae8ed7842001ec601e8191876df715d4fea7394
wujas <loagn2k@gmail.com>
2018-09-12 10:28:51 +0200

    wyjscie-wejscie

---
+++ CPP/wejscie-wyjscie.cxx
@@ -0,0 +1,29 @@
+/*
+ * szablon.cxx
+ */
+
+
+#include <iostream>
+
+using namespace std; 
+
+int main(int argc, char **argv)
+{
+    float a, b; // deklaracja zmiennej
+    a = b = 0; // inicjacja zmiennej
+    
+    cout << "Podaj 1 liczbę: ";
+    cin >> a;
+    cout << a << endl; 
+    
+    cout << "Podaj 2 liczbę: ";
+    cin >> b;
+    cout << b << endl;
+
+    cout << "Suma: " << a + b << endl;
+    cout << "Różnica: " << a - b << endl;
+    cout << "Iloczyn: " << a * b << endl;
+    cout << "Iloraz: " << a / b << endl;
+	return 0;
+}
+
---
+++ CPP/wejscie-wyjscie.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  wejscie-wyjscie.py
+#  
+
+
+
+def main(args):
+    a = int(input("Podaj 1 liczbę: "))
+    print(a)
+    b = int(input("Podaj 2 liczbę: "))
+    print(b)
+  
+    print("Suma: ", a + b)
+    print("Różnica: ", a + b)
+    print("Iloczyn: ", a * b)
+    print("Iloraz: ", a / b)
+    
+    return 0
+
+    # duck typing
+    
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
