ed442186666ffc48128a8d7b766eaf0a01859474
wujas <loagn2k@gmail.com>
2018-09-13 08:33:27 +0200

    funkcje

--- CPP/wejscie-wyjscie.py
+++ CPP/wejscie-wyjscie.py
@@ -4,13 +4,16 @@
 #  wejscie-wyjscie.py
 #  
 
-
+def dodaj():
+    print(a + b)
 
 def main(args):
     a = int(input("Podaj 1 liczbę: "))
     print(a)
     b = int(input("Podaj 2 liczbę: "))
     print(b)
+  
+    dodaj()
   
     print("Suma: ", a + b)
     print("Różnica: ", a + b)
