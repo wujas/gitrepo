ae594484f69778483b006241f3d0e5793b94ddc3
wujas <loagn2k@gmail.com>
2018-09-27 21:40:25 +0200

    teraz juz sobre zadania

---
+++ alogrytm/petla-cw4.py
@@ -0,0 +1,21 @@
+
+#!/usr/bin/env python 
+# -*- coding: utf-8 -*- 
+# 
+#  pentle_cw_4.py 
+# 
+
+
+ 
+
+
+def main(args): 
+
+	for liczba in range(10, 100): 
+		if liczba % 2 ==0 and liczba % 3 == 0: 
+			print (liczba) 
+ 
+
+if __name__ == '__main__': 
+    import sys 
+sys.exit(main(sys.argv)) 
