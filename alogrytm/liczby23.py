7702a8bdb570e310a3b4a19cb16ad366e757975c
wujas <loagn2k@gmail.com>
2018-10-10 21:12:01 +0200

    praca domowa

--- alogrytm/liczby23.py
+++ alogrytm/liczby23.py
@@ -25,9 +25,9 @@ def liczby3():
     licznik=0
     for i in range(1, 10):
         for j in range(10):
-            for k in range(10):
-                if i!=j and j!=k and i!=k:
-                    print(i*100+j*10+k,end=" ")
+            for l in range(10):
+                if i!=j and j!=l and i!=l:
+                    print(i*100+j*10+l,end=" ")
                     licznik+=1
     print()
     return licznik
