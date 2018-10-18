3d520cd607af600d9f79c010dc998de2d9c5c544
wujas <loagn2k@gmail.com>
2018-10-18 08:45:28 +0200

    figury w c++

---
+++ alogrytm/figury.cpp
@@ -0,0 +1,41 @@
+/*
+ * figury.cpp
+ */
+
+
+#include <iostream>
+using namespace std;
+
+
+
+void prostokat(a, b, znak) {
+    
+    for (int i= 0; i < a; i++) {
+        for (int j = 0; j < b; j++) {
+                cout << i *a << j << " ";
+        }
+    
+}
+
+int main(int argc, char **argv)
+{
+    
+    
+    
+    int a, b; // deklaracja 
+    a = b = 0; // inicjacja 
+    //int a = 0; // definicja 
+    cout << "Podaj zakres 1: ";
+    cin >> a;
+    cout << "Podaj zakres 2: ";
+    cin >> b;
+        
+    char znak;
+    cout << "Podaj znak: ";
+    cin >> znak
+    
+    return 0;
+
+	return 0;
+}
+
