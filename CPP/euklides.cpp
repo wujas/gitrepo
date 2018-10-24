6f9635fafab4a7e829ff9caadaf9823b06b94fd8
wujas <loagn2k@gmail.com>
2018-10-24 10:33:48 +0200

    cpp petle

--- CPP/euklides.cpp
+++ CPP/euklides.cpp
@@ -8,16 +8,42 @@
 
 using namespace std;
 
-nwd1()
+int nwd_klasyczny(int a, int b){
+    int licznik = 0;
+    while(a > b) {
+        if(a > b)
+            a = a - b;
+        else
+            b = b - a;
+        licznik++;
+    }
+    cout << "Powtórzeń: " << licznik << endl;
+    return a;
+}
+
+int nwd_optymalny(int a, int b){
+    int licznik = 0;
+    while(a > 0){
+        a = a % b;
+        b = b - a;
+        licznik++;
+    }
+    cout << "Powtórzeń: " << licznik << endl;
+    return b;
+    
+    
+}
+
 int main(int argc, char **argv)
 {
     int a, b;
     cout<< "Podaj 2 liczby: ";
     cin >> a >> b;
-    int nwd = nwd1 (a, b);
+    int nwd = nwd_klasyczny (a, b);
     cout << "NWD(" << a << ","
         << b << ") = "
         << nwd << endl;
+    nwd = nwd_optymalny(a, b);
         
     
     return 0;
---
+++ CPP/minmax/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+void minmax1(){
+    char dalej = 't';
+    int liczba = 0;
+    int min = 0;
+    while (dalej = 't'  {
+        cout << "Podaj liczbę: ";
+    }
+
+}
+int main()
+{
+    cout << "Hello world!" << endl;
+    cout << "No, No Hello world!" << endl;
+    return 0;
+}
