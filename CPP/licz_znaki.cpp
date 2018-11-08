9e9a069cf6b7c0fda6d82c36013a61ca8148cd51
wujas <loagn2k@gmail.com>
2018-11-08 08:45:29 +0100

    cpp

--- CPP/licz_znaki.cpp
+++ CPP/licz_znaki.cpp
@@ -1,48 +1,105 @@
 /*
- * znaki.cpp
+ * licz_znaki.cpp
  */
- 
- 
-#include <iostream> 
+
+
+#include <iostream>
+#include <string.h>
+
+
 using namespace std;
+
+void zamiana_liter(char tab[])
+{   
+    int i = 0;
+    int kod = 0;
+    while(tab[i] != '\0') {
+        kod = (int)tab[i];
+        if (kod >= 65 && kod <= 90)
+            kod += 32;
+        else if (kod >= 97 && kod <= 122)
+            kod -= 32;
+        cout << (char)kod;  
+        i++;
+    }
+}
+
 
 
 void licz_znaki(char tb[], int roz) {
-    //~for(int i = 0; i < roz; i++){
-       //~cout <<  tb[i];
+    //~for(int i=0; i < roz; i++) {
+       //~cout << tb[i];
     //~}
     int i = 0;
     int cyfry, literyD, literyM, reszta;
     cyfry = literyD = literyM = reszta = 0;
-    int znak_kod = 0; // kod ASCII badanego znaku
+    int znak_kod = 0; //kod ASCII badanego znaku
+    
     
     while(tb[i] != '\0'){
         znak_kod = (int)tb[i];
-        if (znak_kod > 64 && znak_kod < 91)
+        if (znak_kod > 64 && znak_kod < 91) // to sprawia że sa duże litery
             literyD++;
         else if (znak_kod > 96 && znak_kod < 123)
             literyM++;
         else if (znak_kod > 47 && znak_kod < 58)
             cyfry++;
-        else
+        else 
             reszta++;
         
-        i++; // inkrementacja indeksu
-    }
-    cout << "Duże: " << literyD << endl;
-    cout << "Małe: " << literyM << endl;
+    i++;  //inkrementcja indeksu
+   }
+    cout << "Duże litery: " << literyD << endl; 
+    cout << "Małe litery: " << literyM << endl; 
     cout << "Cyfry: " << cyfry << endl;
-    cout << "Reszta: " << reszta << endl;
+    cout << "Reszta: " << reszta << endl; 
 }
 
+int zlicz(char tb[]) {
+    int i = 0;
+    while(tb[i] != '\0') i++;
+    return i;
+    
+}
+
+void ascii(char tb[], int roz){
+    int kod = 0;
+    for(int i = 0; i < roz; i++) {
+        kod = (int)tb[i];
+        if (kod > 96 && kod < 123)
+            cout << (char)(kod-32) << " ";
+        else if (kod > 64 && kod < 91)
+            cout << (char)(kod+32) << " ";
+        else
+            cout << tb[i] << " ";
+        
+    }
+}
+
+void odwroc(char[tb], int roz) {
+    int kod = 0;
+    for(int i = 0; i < roz; i++) {
+    
+    
+    
+    }
+    
+    
+}
 
 int main(int argc, char **argv)
 {
     const int rozmiar = 20;
-    char znaki[rozmiar];
+	char znaki[rozmiar];
     cin.getline(znaki, rozmiar);
-    licz_znaki(znaki, cin.gcount());
-    return 0;
+    // licz_znaki(znaki, cin.gcount());
+    int ilosc = 0;
+    // cin.gcount();
+    // ilosc = zlicz(znaki);
+    ilosc = strlen(znaki);
+    // cout << ilosc;
+    ascii (znaki, ilosc);
+    // zamiana_liter(znaki);
+    odwroc (znaki, ilosc);
+	return 0;
 }
-
-
