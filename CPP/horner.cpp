540e93a6df478caffd0a47f34db04bfcdf84c2cb
Szczebrzeszyn <krystian.p20@interia.pl>
2018-11-30 14:18:59 +0100

    turtle

--- CPP/horner.cpp
+++ CPP/horner.cpp
@@ -6,13 +6,30 @@
  * 
  */
 
-
 #include <iostream>
 using namespace std;
 
-void drukujw(int st, float tb[]){
-
+void drukujw(int st, float tb[]) {
+    // 2x^3 + 3x^2 + 5x + 4 
+    int i;
+    for(i = 0; i < st; i++) 
+        cout << tb[i] << "x^" << st-i << " + ";
+    cout << tb[i] << endl;
+        
     }
+
+float horner_it(float x, int stopien, float tbwsp[]){
+    // x(x(2x+ 3) + 5) + 4
+    float wynik;
+    wynik = tbwsp[0];
+    
+    for(int i = 1; i < stopien + 1; i++)
+        wynik = wynik * x + tbwsp[i];
+    
+    
+    
+    return wynik;
+     }
 
 
 int main(int argc, char **argv)
@@ -24,7 +41,7 @@ int main(int argc, char **argv)
     cin >> stopien;
     
     tbwsp = new float [stopien + 1];
-    cout << tbwsp;
+    //cout << tbwsp;
     
     for(int i=0; i <= stopien; i++){
         cout << "Podaj współczynnik przy najwyższej potędze " << stopien-i << ": ";
@@ -34,9 +51,9 @@ int main(int argc, char **argv)
     cout << "Argument:  ";
     cin >> x;
     
-    cout << "Wartość wielomianu o postaci: "
+    cout << "Wartość wielomianu o postaci: ";
     drukujw(stopien, tbwsp);
-    
+    cout << "wynosi: " << horner_it(x, stopien, tbwsp);
     
 	return 0;
 }
---
+++ python/slonce.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  slonce.py
+#  
+
+from turtle import *
+color('red', 'black')
+begin_fill()
+forward(200)
+right(90)
+forward(200)
+right(90)
+forward(200)
+right(90)
+forward(200)
+right(90)
+end_fill()
+done()
