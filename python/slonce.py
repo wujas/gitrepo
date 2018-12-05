f010dc01615de800e09d03857aa62a6f669a2dc4
wujas <loagn2k@gmail.com>
2018-12-05 10:34:07 +0100

    rekurencja

--- python/slonce.py
+++ python/slonce.py
@@ -4,16 +4,33 @@
 #  slonce.py
 #  
 
-from turtle import *
-color('red', 'black')
-begin_fill()
-forward(200)
-right(90)
-forward(200)
-right(90)
-forward(200)
-right(90)
-forward(200)
-right(90)
-end_fill()
-done()
+import turtle as t
+
+def figura(bok, kat, ile):
+    for i in range(ile):
+        t.forward(bok)
+        t.right(kat)
+
+
+def figura_rek(bok, kat, ile, krok):
+    # ~t.right(kat)
+    t.forward(bok)
+    t.right(kat)
+    print(ile)
+    if ile > 0: # warunek brzegowy
+        figura_rek(bok, kat + krok, ile - 1, krok)
+    
+    
+def main():
+    t.setup(800, 600)
+    t.color('red', 'black')
+    t.begin_fill()
+    t.speed(5)
+    
+    figura_rek(200, 50, 18, 10)
+    
+    t.end_fill()
+    t.done()
+    return 0
+
+main()
