44752a4f06aa07aec98b6156a6166d29d2195df3
wujas <loagn2k@gmail.com>
2018-12-14 13:29:21 +0100

    sortowanie

--- CPP/sorowanie.cpp
+++ CPP/sorowanie.cpp
@@ -27,29 +27,45 @@ void drukuj(int tab[], int roz){
     cout << endl;
 }
 
-void zamien1(int a, int b){
-    cout << a << " " << b << endl;
+void zamien1(int &a, int &b){
+    //~cout << a << " " << b << endl;
     int tmp = a;
     a = b;
     b = tmp;
-    cout << a << " " << b << endl;
+    //~cout << a << " " << b << endl;
+    
+}    
+
+void zamien2(int &a, int &b){
+    //~cout << a << " " << b << endl;
+    int tmp = a;
+    a = b;
+    b = tmp;
+    //~cout << a << " " << b << endl;
     
 }    
     
     
     
 void sort_bubble(int tab[], int n) {
-    for (;;) {
-        for(;;) {
-            //~if (tab[i] > tab[i + 1];)
-                //~zamien1(tab[i], tab[i + 1])
+    for (int j = n - 1; j > 0; j--) {
+        for(int i = 0; i < j; i++) {
+            if (tab[i] > tab[i+1])
+                zamien1(tab[i], tab[i + 1]);
         }
     }
-    
-    
 }
 
+void sort_bubbl2(int tab[], int n) {
+    for (int j = n - 1; j > 0; j--) {
+        for(int i = 0; i < j; i++) {
+            if (tab[i] < tab[i+1])
+                zamien1(tab[i], tab[i + 1]);
+        }
+    }
+}
 
+sort_insert
 
 int main(int argc, char **argv)
 {
@@ -58,10 +74,16 @@ int main(int argc, char **argv)
     wypelnij(tab, roz);
     drukuj(tab, roz);
     cout << endl;
-    tab[0] = 7;
-    tab[1] = 5;
-    zamien1(tab[0], tab[1]);
-    cout << tab[0] << " " << tab[1];
+    //~tab[0] = 7;
+    //~tab[1] = 5;
+    //~zamien1(tab[0], tab[1]);
+    //~cout << tab[0] << " " << tab[1];
+    sort_bubble(tab, roz);
+    drukuj(tab, roz);
+    cout << endl;
+    sort_bubbl2(tab, roz);
+    drukuj(tab, roz);
+    
     return 0;
 }
 
