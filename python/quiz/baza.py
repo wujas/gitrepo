9f7fd76da74d2b56a81ac62f723288506469624f
wujas <loagn2k@gmail.com>
2018-12-17 08:45:00 +0100

    quiz

--- python/quiz/baza.py
+++ python/quiz/baza.py
@@ -1,26 +1,59 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-#  baza.py
-#  
-#  Copyright 2018  <>
+#  orm_peewee.py
 
+import os
+from modele import *
+import csv
 
-
+def dane_z_pliku(nazwa_pliku, separator=','):
+    
+    dane = []  # pusta lista na dane
+    
+    if not os.path.isfile(nazwa_pliku):
+        print("Plik {} nie istnieje.".format(nazwa_pliku))
+        return dane
+    
+    with open(nazwa_pliku, 'r', newline='', encoding='utf-8') as plik:
+        tresc = csv.reader(plik, delimiter=separator)
+        for rekord in tresc:
+            rekord = [x.strip() for x in rekord]
+            dane.append(rekord)
+    
+    return dane
 
 
 def dodaj_dane(dane):
-
+    
     for model, plik in dane.items():
         pola = [pole for pole in model._meta.fields]
-        pola.pop(0)
-        wpisy = dane_z_pliku(plik + '.csv')
-        with baza.atomic():
-            model.insert_many(wpisy, fields=pola).execute()
-            
+        pola.pop(0)  
+        
+        wpisy = dane_z_pliku(plik + '.csv', ';')
+                
+        model.insert_many(wpisy, fields=pola).execute()
+
+
 def main(args):
+    
+    if os.path.exists(baza_plik):
+        os.remove(baza_plik)
+
+    baza.connect() #połączenie z bazą
+    baza.create_tables([Kategoria, Pytanie, Odpowiedz])
+
+    dane = {
+        Kategoria: 'kategorie',
+        Pytanie: 'pytania',
+        Odpowiedz: 'odpowiedzi',
+    }
+    
+    dodaj_dane(dane)
+
+    baza.close()
     return 0
 
 if __name__ == '__main__':
     import sys
-    sys.exit(main(sys.argv))
+sys.exit(main(sys.argv))
---
+++ python/quiz/modele.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  modele.py
+#  
+
+from peewee import *
+
+baza_plik = 'quiz.db'
+baza = SqliteDatabase(baza_plik) #określanie instancji bazy
+
+
+### MODELE DANYCH ###
+
+class BazaModel(Model):
+    class Meta :
+        database = baza
+
+class Kategoria(BazaModel): 
+    kategoria = CharField(null=False)
+
+class Pytanie(BazaModel):
+    pytanie = CharField(null=False)
+    kategoria = ForeignKeyField(Kategoria, related_name='pytania')
+
+class Odpowiedz(BazaModel):
+    odpowiedz = CharField(null=False)
+    pytanie = ForeignKeyField(Pytanie, related_name='odpowiedzi')
+    odpok = IntegerField(default=0)
