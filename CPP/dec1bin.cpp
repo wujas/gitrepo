6a518502491c71e2fbfb0c488a26e97cfd57dc62
wujas <loagn2k@gmail.com>
2019-01-31 07:53:41 +0100

    systemy liczbowe

---
+++ CPP/dec1bin.cpp
@@ -0,0 +1,30 @@
+/*
+ * dec1bin.cpp
+ */
+
+
+#include <iostream>
+using namespace std;
+
+
+void dec2bin(int l){
+    ;
+    
+    
+    
+}
+
+void bin2dec(int tab[]){
+    ;
+    
+    
+    
+}
+
+
+int main(int argc, char **argv)
+{
+    
+    return 0;
+}
+
