cfa3c230fa1ad7e90043408514b0cd8b1396153a
wujas <loagn2k@gmail.com>
2019-02-25 08:45:19 +0100

    cos

---
+++ public_html/first.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="pl">
+<!--
+   first.php
+   
+   
+   
+-->
+
+<head>
+<meta charset="utf-8" />
+<title>bez nazwy</title>
+
+</head>
+<body>
+<?php
+echo "Witaj!";
+// zmienne globalne 
+$x = 5 * 5; // zmienna 
+$txt = "działania";
+
+function myTest() {
+    global $x, $txt;
+    if ($x > 20)
+        echo "<p>Wynik ".$txt.": $x</p>";  
+}
+
+myTest();
+
+?>
+
+
+</body>
+
+</html>
