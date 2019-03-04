cf1b4acb5ad16f1916ce88a4a1a3144738408d99
wujas <loagn2k@gmail.com>
2019-03-04 07:48:17 +0100

    d

--- publichtlm2/first.php
+++ publichtlm2/first.php
@@ -2,25 +2,34 @@
 <html lang="pl">
 <!--
    first.php
+   
+   
+   
 -->
 
 <head>
-    <meta charset="utf-8" />
-    <title>bez nazwy</title>
+<meta charset="utf-8" />
+<title>bez nazwy</title>
+
 </head>
 <body>
 <?php
 echo "Witaj!";
-$x = 5 * 5;
+// zmienne globalne 
+$x = 5 * 5; // zmienna 
 $txt = "działania";
 
-function myTest(){
+function myTest() {
     global $x, $txt;
     if ($x > 20)
-        echo"<p>Wynik ".$txt.": $x</p>";    
+        echo "<p>Wynik ".$txt.": $x</p>";  
 }
+
+myTest();
 
 ?>
 
+
 </body>
+
 </html>
--- publichtlm2/index.php
+++ publichtlm2/index.php
@@ -6,6 +6,16 @@ ini_set('display_errors', 1);
 ini_set('log_errors', 1);
 ini_set('error_log', 'errorlog.txt');
 define('DINC', 'inc/');
+define('DBASE', 'db/');
 require_once(DINC.'functions.php');
+
+require_once(DBASE.'db.php');
+$dbfile=DBASE. 'db.sqlite';
+init_baza($dbfile);
+db_exec($inistr);
+
+$id='witam';
+if (isset($_GET['id'])) $id=trim($_GET['id']);
+
 include_once(DINC.'template.php');
 ?>
